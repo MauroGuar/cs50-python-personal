4807ea8fb424adfa96a6cca245daf6f8f7d84590
MauroGuar <m.guarnieri@um.edu.ar>
2024-12-09 00:02:08 -0300

    Finish readability.py

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,56 @@
+from gc import is_finalized
+from operator import index
+from re import split as re_split
+
+from cs50 import get_string
+
+
+def get_number_letters(text: str) -> int:
+    letters = 0
+    for letter in text:
+        if letter.isalpha():
+            letters += 1
+    return letters
+
+
+def get_number_sentences(text: str) -> int:
+    sentences = 0
+    for sentence in re_split('[.!?]', text):
+        if sentence.strip() != '':
+            sentences += 1
+    return sentences
+
+
+def get_number_words(text: str) -> int:
+    words = 0
+    for word in text.split():
+        if word.strip() != '':
+            words += 1
+    return words
+
+
+def main():
+    while True:
+        text = get_string("Text: ")
+        if text.strip() != "":
+            break
+
+    number_of_letters = get_number_letters(text)
+    number_of_sentences = get_number_sentences(text)
+    number_of_words = get_number_words(text)
+
+    l = (number_of_letters / number_of_words) * 100
+    s = (number_of_sentences / number_of_words) * 100
+    index = round(0.0588 * l - 0.296 * s - 15.8)
+
+    if index <= 1:
+        grade = 1
+    elif index <= 16:
+        grade = round(index)
+    else:
+        grade = "16+"
+    print(grade)
+
+
+if __name__ == "__main__":
+    main()
